701af94662b04f5f69cb6ebb60ed2d1c25b13957
nika328 <63118962+nika328@users.noreply.github.com>
2021-12-07 11:26:25 +0100

    Update Fractions_sum.c

--- Fractions_sum.c
+++ Fractions_sum.c
@@ -5,29 +5,9 @@
 #include <string.h>
 
 int main(void) {
-  char string [100];
-  printf("Input two fractions, in the form N1/D1 N2/D2:\n");
-  fgets(string, 100, stdin);
-  char num [100];
-  
-  int count=0, variable=0;
-  
-  int i=0;
-  char *arr[100];
-  arr[i] = strtok(string,"/ ");
-  while(arr[i]!=NULL)
-  {
-    arr[++i] = strtok(NULL,"/ ");
-  }
-
-    num[variable]=string[i];
-    variable++;
-  
-  int n1,n2, d1, d2;
-  n1=atoi(arr[0]);
-  n2=atoi(arr[2]);
-  d1=atoi(arr[1]);
-  d2=atoi(arr[3]);
+  int n1, n2, d1, d2;
+  printf("Enter 2 fractions to be added: ");
+  scanf("%d/%d %d/%d", &n1, &d1, &n2, &d2);
   int d=d1*d2;
   int n=(n1*d2)+(d1*n2);
   int res=-1;
@@ -52,10 +32,6 @@ int main(void) {
     if(d>n)
     {
       d-=n;
-    }
-    if(n==d)
-    {
-      res=n;
     }
   }
   
